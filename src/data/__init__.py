a4623918d32c41b8fdfda3eb4267e9713610bf54
AbdulHadi17 <hadisaqib2048@gmail.com>
2026-04-15 11:37:33 +0500

    feat: update data module init and expose pipeline components

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,8 +1,11 @@
-# Data fetching and validation modules
+# Data fetching, preprocessing, and sequence-building modules
 from src.data.fetch_openaq import OpenAQClient
 from src.data.fetch_weather import OpenMeteoClient
 from src.data.validators import DataValidator, ValidationReport
 from src.data.ingestion_pipeline import DataIngestionPipeline
+from src.data.preprocess import DataPreprocessor
+from src.data.features import FeatureEngineer
+from src.data.sequence_builder import SequenceBuilder
 
 __all__ = [
     "OpenAQClient",
@@ -10,4 +13,7 @@ __all__ = [
     "DataValidator",
     "ValidationReport",
     "DataIngestionPipeline",
+    "DataPreprocessor",
+    "FeatureEngineer",
+    "SequenceBuilder",
 ]
